4a8459137a566775971d49e17f06f5cb8b58b7e4
Eldor99 <eldor1999@protonmail.com>
2020-11-24 12:25:53 +0500

    make an http post,put,delete request

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const http = new easyHTTP;
 
 // Get POSTs
-const posts = http.get('', function(err,posts){
+// const posts = http.get('', function(err,posts){
+// 	if(err){
+// 		console.log(err);
+// 	}else{
+// 	console.log(posts);
+// 	}
+// })
+
+
+// Create Data
+const data = {
+	title: 'Custom Post',
+	body: 'This is a custom post'
+};
+
+// http.post('', data ,function(err, post) {
+// 		if(err){
+// 			console.log(err);
+// 		}else{
+// 			console.log(post);
+// 		}
+
+// });
+
+// Update Post
+// http.put('',data, function(err, post){
+// 	if(err){
+// 		console.log(err);
+// 	}else{
+// 		console.log(post);
+// 	}
+// })
+
+// Delete Post
+http.delete('',data, function(err, post){
 	if(err){
 		console.log(err);
 	}else{
-	console.log(posts);
+		console.log(post);
 	}
 })
-
